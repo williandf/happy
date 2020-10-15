0d38514251054ef57c4a595504c988332cb9969e
Willian Del Freu <williankeyex@gmail.com>
2020-10-15 18:31:51 -0300

    Finalização Back End e Web

---
+++ backend/src/errors/handler.ts
@@ -0,0 +1,23 @@
+import { ErrorRequestHandler } from 'express';
+import { ValidationError } from 'yup'
+
+interface ValidantionErrors {
+  [key: string]: string[];
+}
+
+const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
+  if (error instanceof ValidationError) {
+    let errors: ValidantionErrors = {};
+
+    error.inner.forEach(err => {
+      errors[err.path] = err.errors;
+    });
+
+    return response.status(400).json({ message: 'Validation fails', errors })
+  }
+  console.error(error);
+
+  return response.status(500).json({ message: 'Internal server error' });
+};
+
+export default errorHandler;
